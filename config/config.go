8b286508558f8f9a4e97ac7c0d5cf92984358092
kewsor0x <kewsor0x@gmail.com>
2024-10-07 01:54:37 -0300

    config file to load env variables

---
+++ config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"bytes"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DB_NAME string
+	DB_USER string
+	DB_PASS string
+	DB_HOST string
+	DB_PORT string
+}
+
+var JwtKey []byte
+
+func GetConfig() Config {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("error loading env file: %v", err)
+	}
+
+	var config Config
+	config.DB_NAME = os.Getenv("DB_NAME")
+	config.DB_USER = os.Getenv("DB_USER")
+	config.DB_PASS = os.Getenv("DB_PASS")
+	config.DB_HOST = os.Getenv("DB_HOST")
+	config.DB_PORT = os.Getenv("DB_PORT")
+
+	// temporal key to check
+	jwtKey := os.Getenv("JWT_KEY")
+	if jwtKey == "" {
+		log.Fatalf("error loading key")
+		os.Exit(1)
+	}
+
+	JwtKey = []byte(jwtKey)
+
+	return config
+}
