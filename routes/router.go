46f5afe00aeeff17c705ac47298b63741d204c8f
kewsor0x <kewsor0x@gmail.com>
2024-10-08 20:25:37 -0300

    protected routes

--- routes/router.go
+++ routes/router.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"goapi/handlers"
+	"goapi/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,22 +12,30 @@ import (
 func InitRouter(db *gorm.DB) *mux.Router {
 	r := mux.NewRouter()
 	userRouter := r.PathPrefix("/users").Subrouter()
+	protectedRouter := r.PathPrefix("/protected").Subrouter()
+	protectedRouter.Use(middlewares.JWTMiddleware)
+
+	r.HandleFunc("/login", handlers.LoginHandler).Methods(http.MethodPost)
 
 	// GET public routes
 	userRouter.HandleFunc("", handlers.GetUser).Methods(http.MethodGet)
-	userRouter.HandleFunc("/{id}", handlers.GetUser).Methods(http.MethodGet)
 
 	// POST routes
 	userRouter.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
 		handlers.CreateUser(db, w, r)
 	}).Methods(http.MethodPost)
 
+	// GET protected routes
+	protectedRouter.HandleFunc("/user/{id}", handlers.GetUser).Methods(http.MethodGet)
+	protectedRouter.HandleFunc("/test", middlewares.ProtectedTest).Methods(http.MethodGet)
+
 	// PUT routes
-	userRouter.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+	protectedRouter.HandleFunc("/user/{id}", func(w http.ResponseWriter, r *http.Request) {
 		handlers.UpdateUser(db, w, r)
 	}).Methods(http.MethodPut)
 
-	userRouter.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+	// DELETE routes
+	protectedRouter.HandleFunc("/user/{id}", func(w http.ResponseWriter, r *http.Request) {
 		handlers.DeleteUser(db, w, r)
 	}).Methods(http.MethodDelete)
 
