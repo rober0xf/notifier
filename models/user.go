7ddefe939f39dca5af9239c11c59089ec4cd89bb
kewsor0x <kewsor0x@gmail.com>
2024-10-08 20:24:00 -0300

    unique email

--- models/user.go
+++ models/user.go
@@ -4,6 +4,6 @@ type User struct {
 	ID       uint   `gorm:"primaryKey" json:"id"`
 	Name     string `gorm:"not null" json:"name"`
 	Username string `json:"username"`
-	Email    string `gorm:"not null" json:"email"`
+	Email    string `gorm:"not null;unique" json:"email"`
 	Password string `gorm:"not null" json:"password,omitempty"`
 }
