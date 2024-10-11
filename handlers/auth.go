1fd51440e8ea71a2ff9c4cd2f5ab1bc70afc5e44
kewsor0x <kewsor0x@gmail.com>
2024-10-11 00:42:44 -0300

    refactoring for html form

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,11 +1,11 @@
 package handlers
 
 import (
-	"encoding/json"
 	"errors"
 	"goapi/config"
 	"goapi/dbconnect"
 	"goapi/models"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
@@ -24,6 +24,11 @@ type jwtclaims struct {
 	Identity string `json:"identity"`
 	ID       string `json:"ID"`
 	jwt.RegisteredClaims
+}
+
+type errorResponse struct {
+	Message string
+	Status  int
 }
 
 func getIDfromToken(tokenString string) string {
@@ -63,44 +68,64 @@ func generateToken(email string, userID string) (string, error) {
 	return tokenString, nil
 }
 
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
+func LoginHandler(w http.ResponseWriter, r *http.Request) *errorResponse {
 	var db = dbconnect.DB
 	var user models.User
 	var credentials fieldsInput
 
 	// check if the fields are valid
 	defer r.Body.Close()
-	err := json.NewDecoder(r.Body).Decode(&credentials)
-	if err != nil {
-		http.Error(w, "invalid request", http.StatusBadRequest)
-		return
+	if err := r.ParseForm(); err != nil {
+		return &errorResponse{"invalid request", http.StatusBadRequest}
 	}
+	/*
+		err := json.NewDecoder(r.Body).Decode(&credentials)
+		if err != nil {
+			return &errorResponse{"invalid request", http.StatusBadRequest}
+		}
+
+	*/
+
+	credentials.Identity = r.FormValue("identity")
+	credentials.Password = r.FormValue("password")
+
+	log.Printf("buscando user con email: %v", credentials.Identity)
 
 	// search for the user in the database
 	if err := db.Where("email = ?", credentials.Identity).First(&user).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			http.Error(w, "invalid credentials", http.StatusNotFound)
-		} else {
-			http.Error(w, "error while connecting", http.StatusInternalServerError)
+            log.Println("llegaste hasta aca?")
+			return &errorResponse{"user not found", http.StatusNotFound}
 		}
-		return
+        log.Printf("error al buscar el usuario: %v", err)
+		return &errorResponse{"error while connecting", http.StatusInternalServerError}
 	}
 
 	// validate the password
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(credentials.Password)); err != nil {
-		http.Error(w, "invalid credentials", http.StatusUnauthorized)
-		return
+		return &errorResponse{"invalid credentials", http.StatusUnauthorized}
 	}
 
 	// take from the token the user.ID
 	var userID string = strconv.FormatUint(uint64(user.ID), 10)
 	tokenResponse, err := generateToken(user.Email, userID)
 	if err != nil {
-		http.Error(w, "error while generating token", http.StatusInternalServerError)
-		return
+		return &errorResponse{"error while generating token", http.StatusInternalServerError}
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"token": tokenResponse})
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session:id",
+		Value:    tokenResponse,
+		Path:     "/",
+		HttpOnly: true, // prevent XSS
+	})
+
+	log.Printf("todo salio bien y vas a ser redireccionado")
+	w.Header().Set("HX-Redirect", "/")
+	/*
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"token": tokenResponse})
+	*/
+	return nil
 }
