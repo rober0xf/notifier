47066b6e11a061c6e4f7f1b88ef2487542624969
kewsor0x <kewsor0x@gmail.com>
2024-10-17 16:41:17 -0300

    refactoring

--- routes/router.go
+++ routes/router.go
@@ -18,36 +18,37 @@ func InitRouter(db *gorm.DB, tmpl *template.Template) *mux.Router {
 	protectedRouter := r.PathPrefix("/protected").Subrouter()
 	categoryRouter := r.PathPrefix("/categories").Subrouter()
 
-
 	protectedRouter.Use(middlewares.JWTMiddleware) // apply the middleware
+	categoryRouter.Use(middlewares.JWTMiddleware)
 
-	// simple routes
 	r.HandleFunc("/", handlers.HomeHandler).Methods(http.MethodGet)
 	r.HandleFunc("/login", handlers.LoginTemplate()).Methods(http.MethodGet)
 	r.HandleFunc("/login", handlers.LoginTemplate()).Methods(http.MethodPost)
 
-	// GET public routes
+	// users
 	userRouter.HandleFunc("", handlers.GetUser).Methods(http.MethodGet)
-	categoryRouter.HandleFunc("", handlers.GetCategories).Methods(http.MethodGet)
-	categoryRouter.HandleFunc("/{id}", handlers.GetCategories).Methods(http.MethodGet)
-
-	// POST routes
 	userRouter.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
 		handlers.CreateUser(db, w, r)
 	}).Methods(http.MethodPost)
-
-	// GET protected routes
 	protectedRouter.HandleFunc("/users/{id}", handlers.GetUser).Methods(http.MethodGet)
-	protectedRouter.HandleFunc("/test", middlewares.ProtectedTest).Methods(http.MethodGet)
-
-	// PUT routes
 	protectedRouter.HandleFunc("/users/{id}", func(w http.ResponseWriter, r *http.Request) {
 		handlers.UpdateUser(db, w, r)
 	}).Methods(http.MethodPut)
-
-	// DELETE routes
 	protectedRouter.HandleFunc("/users/{id}", func(w http.ResponseWriter, r *http.Request) {
 		handlers.DeleteUser(db, w, r)
+	}).Methods(http.MethodDelete)
+
+	// categories
+	categoryRouter.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
+		handlers.CreateCategory(db, w, r)
+	}).Methods(http.MethodPost)
+	categoryRouter.HandleFunc("", handlers.GetCategories).Methods(http.MethodGet)
+	categoryRouter.HandleFunc("/{id}", handlers.GetCategories).Methods(http.MethodGet)
+	categoryRouter.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		handlers.UpdateCategory(db, w, r)
+	}).Methods(http.MethodPut)
+	categoryRouter.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		handlers.DeleteCategory(db, w, r)
 	}).Methods(http.MethodDelete)
 
 	return r
