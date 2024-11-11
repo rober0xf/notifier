ae7ca8d220d25ae366fc7eb19c909d3cc3bbab62
kewsor0x <kewsor0x@gmail.com>
2024-11-11 13:37:37 -0300

    deleted: templates

--- main.go
+++ main.go
@@ -15,11 +15,6 @@ var tmpl *template.Template
 func main() {
 	go cronjob.InitCron()
 
-	if err := LoadTemplates(); err != nil {
-		log.Printf("error loading templates: %v", err)
-		return
-	}
-
 	db, err := dbconnect.Connect()
 	if err != nil {
 		log.Fatalf("could not connect to database: %v", err)
@@ -29,15 +24,4 @@ func main() {
 
 	fmt.Println("running on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", r))
-}
-
-func LoadTemplates() error {
-	var err error
-	tmpl, err = template.ParseGlob("templates/*.templ")
-
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
