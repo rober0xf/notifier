d432690c8161b3fbbb345279fdd0cb10eff9ec0f
rober0xf <robertinopili13@gmail.com>
2025-08-22 16:25:52 -0300

    payment repository

--- internal/ports/payments_interface.go
+++ internal/ports/payments_interface.go
@@ -3,19 +3,19 @@ package ports
 import "github.com/rober0xf/notifier/internal/domain"
 
 type PaymentService interface {
-	Create(*domain.Payment) error
+	Create(*domain.Payment) (*domain.Payment, error)
 	Get(id uint) (*domain.Payment, error)
-	GetAllPayments(user_id uint) ([]*domain.Payment, error)
-	GetPaymentFromID(id uint, user_id uint) (*domain.Payment, error)
+	GetAllPayments(user_id uint) ([]domain.Payment, error)
+	GetPaymentFromIDAndUserID(id uint, user_id uint) (*domain.Payment, error)
 	Update(*domain.Payment) (*domain.Payment, error)
 	Delete(id uint) error
 }
 
 type PaymentRepository interface {
-	Create(payment *domain.Payment) error
-	GetByID(id uint) (*domain.Payment, error)
-	GetAllByUserID(user_id uint) ([]*domain.Payment, error)
-	GetByIDAndUserID(id uint, user_id uint) (*domain.Payment, error)
-	Update(payment *domain.Payment) error
-	Delete(id uint) error
+	CreatePayment(payment *domain.Payment) error
+	GetPaymentByID(id uint) (*domain.Payment, error)
+	GetAllPaymentsByUserID(user_id uint) ([]domain.Payment, error)
+	GetPaymentByIDAndUserID(id uint, user_id uint) (*domain.Payment, error)
+	UpdatePayment(payment *domain.Payment) error
+	DeletePayment(id uint) error
 }
