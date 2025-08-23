c7133228be531a6f2283faf520da0f84f1e91919
rober0xf <robertinopili13@gmail.com>
2025-08-23 20:11:52 -0300

    auth interface

--- internal/ports/auth_interface.go
+++ internal/ports/auth_interface.go
@@ -1,13 +1,21 @@
-package auth
+package ports
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/rober0xf/notifier/internal/adapters/httphelpers/dto"
 	"github.com/rober0xf/notifier/internal/domain"
 )
 
 type AuthService interface {
+	ParseLoginRequest(r *http.Request) (dto.LoginRequest, error)
+	GetUserIDFromRequest(r *http.Request) (uint, error)
+	GenerateToken(userID uint, email string) (string, error)
+	ExistsUser(ctx context.Context, credentials dto.LoginRequest) (*domain.User, error)
+}
+
+type AuthRepository interface {
 	ValidateToken(token_string string) (uint, error)
 	ParseUserFromToken(token_string string) (*domain.User, error)
 	GenerateToken(userID uint, email string) (string, error)
