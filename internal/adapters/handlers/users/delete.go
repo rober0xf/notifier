2392a495d1368fb2954f985f7a17de68e0deb890
rober0xf <robertinopili13@gmail.com>
2025-08-24 21:21:51 -0300

    routes: delete user

---
+++ internal/adapters/handlers/users/delete.go
@@ -0,0 +1,26 @@
+package users
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *userHandler) DeleteUser(c *gin.Context) {
+	id_str := c.Param("id")
+
+	id, err := strconv.Atoi(id_str)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "error parsing id"})
+		return
+	}
+
+	err = h.UserService.Delete(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error deleting user"})
+		return
+	}
+
+	c.JSON(http.StatusNoContent, gin.H{"message": "user deleted successfully"})
+}
