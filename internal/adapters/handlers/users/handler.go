b3bf4d5eadff8ecb289c723e04f13fe4c5c397ce
rober0xf <robertinopili13@gmail.com>
2025-08-29 18:56:15 -0300

    users routes

---
+++ internal/adapters/handlers/users/handler.go
@@ -0,0 +1,17 @@
+package users
+
+import (
+	"github.com/rober0xf/notifier/internal/services/auth"
+	"github.com/rober0xf/notifier/internal/services/users"
+)
+
+type userHandler struct {
+	UserService users.Service
+	Utils       auth.Service
+}
+
+func NewUserHandler(service users.Service) *userHandler {
+	return &userHandler{
+		UserService: service,
+	}
+}
